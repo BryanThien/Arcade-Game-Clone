d09cd3a6675a8fc9be47e5850c9690db03dc996d
Bryan Thien <eonearthproductions@gmail.com>
2018-08-21 15:20:28 -0500

    Makes enemy and player classes

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
+const Enemy = (state) => ({
+// Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    sprite = 'images/enemy-bug.png',
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
+    update: (dt) => {
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
+    },
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    render: () => ctx.drawImage(Resources.get(sprite), x, y)
+});
+    
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+const Player = (state) => ({
+    update: () => {},
+    render: () => {},
+    handleInput: () => {},
+})
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -44,3 +48,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
